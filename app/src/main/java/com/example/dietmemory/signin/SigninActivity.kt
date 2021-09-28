68790f57247bb021c5aff85e940c15062a35f0fc
YSH <sh10302@naver.com>
2021-09-28 16:46:54 +0900

    add rest of layout file

---
+++ app/src/main/java/com/example/dietmemory/signin/SigninActivity.kt
@@ -0,0 +1,8 @@
+package com.example.dietmemory.signin
+
+import com.example.dietmemory.config.BaseActivity
+import com.example.dietmemory.databinding.ActivitySigninBinding
+
+class SigninActivity : BaseActivity<ActivitySigninBinding>(ActivitySigninBinding::inflate) {
+
+}
