9b02f3a9e49bcd3ea02f8db3e2dffbae89db81f9
YSH <sh10302@naver.com>
2021-10-13 14:33:34 +0900

    add retrofit2 function to GlobalApplication.kt

--- app/src/main/java/com/example/dietmemory/config/GlobalApplication.kt
+++ app/src/main/java/com/example/dietmemory/config/GlobalApplication.kt
@@ -2,16 +2,38 @@ package com.example.dietmemory.config
 
 import android.app.Application
 import android.content.SharedPreferences
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class GlobalApplication : Application() {
     companion object {
+        val API_URL = "https://diet-plan.run.goorm.io/"
         lateinit var globalSharedPreferences : SharedPreferences
         const val X_ACCESS_TOKEN = "X-ACCESS-TOKEN"
         const val STRING_DEFAULT = "__DEFAULT__"
+        lateinit var sRetrofit: Retrofit
     }
 
     override fun onCreate() {
         super.onCreate()
+        initRetrofitInstance()
         globalSharedPreferences = applicationContext.getSharedPreferences("dietMemory", MODE_PRIVATE)
+
+    }
+
+    private fun initRetrofitInstance() {
+        val client : OkHttpClient = OkHttpClient.Builder()
+                .readTimeout(5000, TimeUnit.MILLISECONDS)
+                .connectTimeout(5000, TimeUnit.MILLISECONDS)
+                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .addNetworkInterceptor(XAccessTokenInterceptor()).build()
+
+        sRetrofit = Retrofit.Builder()
+                .baseUrl(API_URL).client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
     }
 }
---
+++ app/src/main/java/com/example/dietmemory/config/XAccessTokenInterceptor.kt
@@ -0,0 +1,18 @@
+package com.example.dietmemory.config
+
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
+
+class XAccessTokenInterceptor : Interceptor {
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val builder : Request.Builder = chain.request().newBuilder()
+        val jwtToken : String ?= GlobalApplication.globalSharedPreferences.getString(GlobalApplication.X_ACCESS_TOKEN, null)
+        if (jwtToken != null){
+            builder.addHeader("x-access-token", jwtToken)
+        }
+        return chain.proceed(builder.build())
+    }
+}
