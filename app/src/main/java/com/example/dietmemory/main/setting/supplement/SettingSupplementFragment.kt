f9e7e26a758fefb37402beee3c51cce44086d037
YSH <sh10302@naver.com>
2021-10-26 20:03:44 +0900

    fix error

--- app/src/main/java/com/example/dietmemory/main/setting/supplement/SettingSupplementFragment.kt
+++ app/src/main/java/com/example/dietmemory/main/setting/supplement/SettingSupplementFragment.kt
@@ -5,7 +5,7 @@ import com.example.dietmemory.config.BaseFragment
 import com.example.dietmemory.databinding.FragmentMainSettingSupplementBinding
 import com.example.dietmemory.main.setting.SettingDetailContract
 
-class SettingSupplementFragment : BaseFragment<FragmentMainSettingSupplementBinding>(FragmentMainSettingSupplementBinding::bind, R.layout.fragment_main_setting_restrict), SettingDetailContract.View {
+class SettingSupplementFragment : BaseFragment<FragmentMainSettingSupplementBinding>(FragmentMainSettingSupplementBinding::bind, R.layout.fragment_main_setting_supplement), SettingDetailContract.View {
     override fun save() {
         // some save function
     }
